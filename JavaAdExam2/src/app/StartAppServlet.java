983297273a8aa0ad1c1d218a9000e814ffbe2320
Cao Gia Huy <caogiahuy13@gmail.com>
2020-07-29 17:27:56 +0900

    Java応用演習問題2_基本

---
+++ JavaAdExam2/src/app/StartAppServlet.java
@@ -0,0 +1,55 @@
+package app;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StartAppServlet
+ */
+@WebServlet("/StartAppServlet")
+public class StartAppServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StartAppServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/appStart.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+
+		String result = "";
+
+		if (name != null && !name.isEmpty()) {
+			GameApp gameApp = new GameApp("何か");
+			result = gameApp.start(name);
+		}
+
+		request.setAttribute("result", result);
+		request.getRequestDispatcher("/appStart.jsp").forward(request, response);
+	}
+
+}
